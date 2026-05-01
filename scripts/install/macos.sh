b30f7bb97a68379911ca78327780be5a6044eb2e
yoshikt <yoshikazu.katayama@gmail.com>
2026-05-01 19:17:32 +0900

    iTerm2 のログ設定を整理

--- scripts/install/macos.sh
+++ scripts/install/macos.sh
@@ -38,7 +38,7 @@ _apply_iterm2_settings() {
     local source_path="${DOTFILES_DIR}/macos/iterm2/com.googlecode.iterm2.plist"
     local target_path="${HOME}/.config/iterm2/com.googlecode.iterm2.plist"
     local prefs_folder="${DOTFILES_DIR}/macos/iterm2"
-    local log_dir="${HOME}/.local/state/terminallogs"
+    local log_dir="${HOME}/.local/state/iterm2/logs"
     local current_prefs_folder
     local current_load_custom
 
